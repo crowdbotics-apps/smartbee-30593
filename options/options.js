f1dca4a75a844058d4a76b408c00c22379f8d0bc
Crowdbotics <team@crowdbotics.com>
2021-09-16 13:18:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "smartbee_30593Identifier",
+  url: "https://smartbee_30593Identifier.botics.co",
+  api: "https://smartbee_30593Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
